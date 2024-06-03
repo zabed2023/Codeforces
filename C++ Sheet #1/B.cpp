ef05621c3c30ae651b97d14598583b9223f15b68
Zabed <142087300+zabed2023@users.noreply.github.com>
2024-06-03 07:49:18 +0600

    Create B.cpp

---
+++ C++ Sheet #1/B.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    int in;
+    long long ln;
+    char ch;
+    float fl;
+    double db;
+
+    cin>> in >> ln >> ch >> fl >> db;
+
+    cout<< in<<endl << ln<<endl << ch << endl << fl << endl<< db;
+
+    getch();
+
+
+
+}
+
