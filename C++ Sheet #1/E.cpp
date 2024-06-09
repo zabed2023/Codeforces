4225165e315220c33ea81cdf4647292a4bd2cbfb
Zabed <142087300+zabed2023@users.noreply.github.com>
2024-06-09 20:46:45 +0600

    Create E.cpp

---
+++ C++ Sheet #1/E.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+    cout<<fixed<<setprecision(9);
+    double r;
+
+    cin>> r;
+
+    double area=3.141592653*r*r;
+
+    cout<<area;
+
+    return 0;
+
+
+
+
+
+}
