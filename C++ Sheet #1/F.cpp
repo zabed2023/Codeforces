e755dc7b10f9fff872cbab63bdadb2a35c84e0c6
Zabed <142087300+zabed2023@users.noreply.github.com>
2024-06-10 15:20:15 +0600

    Create F.cpp

---
+++ C++ Sheet #1/F.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    long long int a,b,x,y;
+
+    cin>>a>>b;
+
+    x= a%10;
+    y=b%10;
+    long long int sum=x+y;
+
+    cout<< sum;
+
+
+
+    return 0;
+}
