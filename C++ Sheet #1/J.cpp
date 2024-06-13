4d9c90baeb2787c756fa8cdea2ec0cb0e02edcda
Zabed <142087300+zabed2023@users.noreply.github.com>
2024-06-13 11:29:07 +0600

    Add files via upload

---
+++ C++ Sheet #1/J.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    long long int a,b;
+
+    cin>>a>>b;
+
+    if(a%b==0 || b%a==0)
+    {
+        cout<< "Multiples";
+    }
+    else
+    {
+        cout<< "No Multiples";
+    }
+
+
+
+    return 0;
+}
